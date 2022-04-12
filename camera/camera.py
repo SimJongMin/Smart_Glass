661dfd0da1c7d4f94905edc283a0b9e4ed160f1b
SimJongMin <86102735+SimJongMin@users.noreply.github.com>
2022-04-12 18:17:01 +0900

    Add files via upload

---
+++ camera/camera.py
@@ -0,0 +1,9 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(3)
+camera.capture('/home/pi/camera/images/captured.png')
+camera.stop_preview()
