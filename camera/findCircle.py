83e10dd57af071f3e23830d23e70f4f1056dbc42
SimJongMin <86102735+SimJongMin@users.noreply.github.com>
2022-05-09 01:34:20 +0900

    원 개수 중복 제거할라고 수정중

--- camera/findCircle.py
+++ camera/findCircle.py
@@ -17,8 +17,10 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # image = cv2.GaussianBlur(image, (0, 0), 1)
 # cimage = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1,
-                           1, param1=90, param2=12, minRadius=0, maxRadius=10)
+                           1, param1=90, param2=14, minRadius=0, maxRadius=10)
 circles = np.uint16(np.around(circles))
+
+"""
 for i in circles[0]:
     cv2.circle(dst, (i[0], i[1]), i[2], (0, 255, 0), 2)
 
@@ -26,6 +28,44 @@ print(circles.shape[1])
 cv2.imshow("dst", dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+"""
+
+# 원 개수
+circle_count = 0
+
+# 겹치는 원 해결하기 위한 리스트 x, y좌표 +-2 이용
+overlap_circle_x = [0]
+overlap_circle_y = [0]
+
+"""
+아니 이거 삼중포문 하니까 라즈베리파에서 안돌아갈거같음
+리스트 안에 x,y 좌표 넣어서 +-2이면 겹치는 거니까 안그리고 넘어갈라고 생각했는데
+뭐 어케해야되는지를 모르겠네
+파이썬 공부좀 할걸
+위에 주석처리 해놓은게 원래 코드
+응용해서 해보는데 어렵네
+"""
+    
+for i in circles[0]:
+    print(i)
+    print("x값", i[0])
+    for j in overlap_circle_x:
+        print("j값", j)
+        for q in overlap_circle_y:
+            print("q값", q)
+            if not (j-3 < i[0] < j+3 and q-3 < i[1] < q+3):
+                cv2.circle(dst, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                overlap_circle_x.append(i[0])
+                overlap_circle_y.append(i[1])
+                circle_count += 1
+                break
+print("원 개수", circle_count)
+
+#print(circles.shape[1])
+cv2.imshow("dst", dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 
 
 
