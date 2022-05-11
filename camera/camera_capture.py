a25bf3c8f892ff95cc8e190d473dd70110a796d5
최인식 <66933869+cislove98@users.noreply.github.com>
2022-05-11 23:25:12 +0900

    카메라 문제 해결

--- camera/camera_capture.py
+++ camera/camera_capture.py
@@ -1,20 +1,6 @@
-from picamera import PiCamera
 from time import sleep
+import os
 
-"""
-camera = PiCamera()
-
-camera.start_preview()
-sleep(5)
-camera.capture('/home/pi/camera/images/captured.jpg')
-camera.stop_preview()
-"""
 
 def camera():
-        camera = PiCamera()
-
-        camera.start_preview()
-        sleep(5)
-        camera.capture('/home/pi/camera/images/captured.jpg')
-        camera.stop_preview()
-        
+        os.system("libcamera-still -o ./images/captured.jpg")
